e655ee3b39cef4cdae291c929d82e89a8b264289
Sergei <sergei.nikonov@yahoo.com>
2018-03-21 20:28:16 +0200

    css issues fixed

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -2,167 +2,122 @@
 
 @section('main-content')
 
-    <main>
-        <div class="block-photo-text">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12 col-md-4">
-                        <div class="left-block-img">
-                            <div class="picture-studio">
-                                <img class="img-responsive" src="img/photo-studio.png" alt="">
-                                <div class="film-strip"><img src="img/icons/film-strip.png" alt="" class="img-responsive"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-md-8">
-                        <div class="photo-text">
-                            <h2>We are a creative film and video production company.</h2>
-                            <div class="line-text"></div>
-                            <p>{{ $settings->first_block_text }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="main-block">
-            <div class="main-block-icons">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="filming">
-                                <h2 id="film">filming services</h2>
-                                <div class="line-filming"></div>
-                                <p>{{ $settings->second_block_text }}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="container">
-                    <div class="row no-gutters">
-                        <div class="block-icons-top">
-                            <div class="col-xs-12 col-md-6">
-                                <div class="commercial">
-                                    <div class="commercial-h2">
-                                        <h2>commercial</h2>
-                                    </div>
-                                    <div class="img-commerc">
-                                        <img src="img/icons/icons-commercial.png" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-md-6">
-                                <div class="music-animation">
-                                    <div class="music-h2">
-                                        <h2>music&animation</h2>
-                                    </div>
-                                    <div class="music-img">
-                                        <img src="img/icons/icons-animation.png" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row no-gutters">
-                        <div class="block-icons-buttom">
-                            <div class="col-xs-12 col-md-6">
-                                <div class="corporate">
-                                    <div class="corporate-h2">
-                                        <h2>corporate</h2>
-                                    </div>
-                                    <div class="corporate-img">
-                                        <img src="img/icons/icons-corporate.png" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-md-6">
-                                <div class="non-profit">
-                                    <div class="non-profit-h2">
-                                        <h2>non-profit</h2>
-                                    </div>
-                                    <div class="non-profit-img">
-                                        <img src="img/icons/icons-nonprofit.png" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="buttom-main hovers">
-                        <a href="#form">hire us</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="main-conteiner">
-            <h2>latest works</h2>
-            <div class="container">
-                <div class="row no-gutters">
-                    <div class="block-photo">
-                        <div class="col-md-7">
-                            <div class="left-block-photo">
-                                <div class="photo-cars">
-                                    <img class="img-responsive" src="img/cars-photo.png" alt="">
-                                </div>
-                                <div class="picture-line">
-                                    <div class="col-md-6">
-                                        <div class="people-mask">
-                                            <img class="img-responsive" src="img/peoples-mask.png" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="wooman">
-                                            <img class="img-responsive" src="img/woomen.png" alt="" class="responsive">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-5">
-                            <div class="right-block-photo">
-                                <div class="sport-game">
-                                    <div>
-                                        <img class="img-responsive" src="img/sport-game_big.png" alt="">
-                                    </div>
-                                    <div class="sport-text">
-                                        <h2>green gray</h2>
-                                        <p>{{ $settings->third_block_text }}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-    <div id="myModal" class="modal fade" role="dialog">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-          <div class="modal-body">
-            <div class="row">
-              <div class="owl-carousel owl-theme">
-                <div class="item-video">
-                  <a class="owl-video" href="https://www.youtube.com/watch?v=0EMowVnDrsU"></a>
-                </div>
-                <div class="item-video">
-                  <a class="owl-video" href="https://www.youtube.com/watch?v=hnHz8ogAGpk"></a>
-                </div>
-                <div class="item-video">
-                  <a class="owl-video" href="https://vimeo.com/146103933"></a>
-                </div>
-                <div class="item-video">
-                  <a class="owl-video" href="https://vimeo.com/177937692"></a>
-                </div>
-                <div class="item-video">
-                  <a class="owl-video" href="https://vimeo.com/179098094"></a>
-                </div>
+<main>
+  <div class="container">
+    <div class="block-photo-text">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-4">
+            <div class="left-block-img">
+              <div class="picture-studio">
+                <img class="img-responsive" src="img/photo-studio.png" alt="">
+                <div class="film-strip"><img src="img/icons/film-strip.png" alt="" class="img-responsive"></div>
               </div>
             </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" onclick="hideModal()">Close</button>
+          <div class="col-xs-12 col-md-8">
+            <div class="photo-text">
+              <h2>We are a creative film and video production company.</h2>
+              <div class="line-text"></div>
+              <p>{{ $settings->first_block_text }}</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
+  <div class="main-block">
+    <div class="main-block-icons">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="filming">
+              <h2 id="film">filming services</h2>
+              <div class="line-filming"></div>
+              <p>{{ $settings->second_block_text }}</p> 
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="block-icons-top">
+            <div class="col-xs-12 col-md-6">
+              <div class="commercial">
+                <div class="commercial-h2">
+                  <h2>commercial</h2>
+                </div>
+                <div class="img-commerc">
+                  <img src="img/icons/icons-commercial.png" alt="">
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-md-6">
+              <div class="music-animation">
+                <div class="music-h2">
+                  <h2>music&animation</h2>
+                </div>
+                <div class="music-img">
+                  <img src="img/icons/icons-animation.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="block-icons-buttom">
+            <div class="col-xs-12 col-md-6">
+              <div class="corporate">
+                <div class="corporate-h2">
+                  <h2>corporate</h2>
+                </div>
+                <div class="corporate-img">
+                  <img src="img/icons/icons-corporate.png" alt="">
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-md-6">
+              <div class="non-profit">
+                <div class="non-profit-h2">
+                  <h2>non-profit</h2>
+                </div>
+                <div class="non-profit-img">
+                  <img src="img/icons/icons-nonprofit.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="buttom-main hovers">
+          <a href="#form">hire us</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div id="latest" class="main-conteiner">
+      <h2>latest works</h2>
+      <div class="row">
+        <div class="owl-carousel owl-theme">
+          <div class="item-video">
+            <a class="owl-video" href="https://www.youtube.com/watch?v=0EMowVnDrsU"></a>
+          </div>
+          <div class="item-video">
+            <a class="owl-video" href="https://www.youtube.com/watch?v=hnHz8ogAGpk"></a>
+          </div>
+          <div class="item-video">
+            <a class="owl-video" href="https://vimeo.com/146103933"></a>
+          </div>
+          <div class="item-video">
+            <a class="owl-video" href="https://vimeo.com/177937692"></a>
+          </div>
+          <div class="item-video">
+            <a class="owl-video" href="https://vimeo.com/179098094"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
 
 @stop
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,104 +1,115 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" type="text/css" href="fonts/font.css">
-        <link rel="stylesheet" type="text/css" href="fonts/stylesheet.css">
-        <link rel="stylesheet" href="css/owl.carousel.min.css">
-        <link rel="stylesheet" href="css/owl.theme.default.min.css">
-        <title>RedRoom</title>
-    </head>
-    <body>
-        <div class="container">
-            <header>
-                <div class="block-header">
-                    <div>
-                        <img class="img-responsive" src="img/main_pic.png" alt="">
-                    </div>
-                    <div class="navbar">
-                        <div class="nav-menu">
-                            <div class="left-prev">
-                                <img src="img/icons/left-prev.png" alt="" class="img-responsive">
-                            </div>
-                            <div class="menu_line">
-                                <ul>
-                                    <li><a href="#film">film</a></li>
-                                    <li><a href="#film">video</a></li>
-                                    <li><a href="#film">animation</a></li>
-                                </ul>
-                            </div>
-                            <div class="right-prev"><img src="img/icons/right-prev.png" alt="" class="img-responsive"></div>
-                        </div>
-                        <div class="line-buttom">
-                            <div class="left-buttom hovers btn-danger">
-                                <a onclick="showModal()">view projects</a>
-                            </div>
-                            <div class="right-buttom hovers btn-danger">
-                                <a href="#form">hire us</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </header>
-
-            @yield('main-content')
-
-            <footer>
-                <div class="footer-block">
-                    <div class="container">
-                        <div class="row no-gutters">
-                            <div class="col-xs-12">
-                                <div class="footer-text text-center">
-                                    <p>So What's Next?</p>
-                                    <h2>Let’s Work! <u>Just drop us a line.</u></h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="container">
-                    <div class="row no-gutters">
-                        <div class="col-xs-12">
-                            <div class="img-footer">
-                                <div class="form gradient">
-                                    <form id="form">
-                                        <p>Hello, my name is <input type="text" class="decorate" id="msgName" placeholder="first&last name">.</p>
-                                        <p> I need a <input type="text" class="decorate" id="msgType" placeholder="films, video, animation,">.</p>
-                                        <p>approximate duration around <input type="text" class="decorate" id="msgDuration" placeholder="xx:xx">.</p>
-                                        <p>My budget is, <input type="text" class="decorate" id="msgBudget" placeholder="1000$"></p>
-                                        <p>You can reach me on my phone at <input type="text" class="decorate" id="msgTelephone" placeholder="xxx-xxx-xxx"></p>
-                                        <p>Or via email at <input type="text" class="decorate" id="msgEmail" placeholder="your@email.com"></p>
-                                    </form>
-                                    <div class="send-btn">
-                                        <div class="send-img" onclick="sendEmail()">
-                                            <img src="img/icons/air_send.png" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="line-icons-footer">
-                                        <ul>
-                                            <li><a href="{{ $settings->insta_link }}"><img src="img/icons/instagram.png" alt=""></a></li>
-                                            <li><a href="{{ $settings->youtube_link }}"><img src="img/icons/youtube.png" alt=""></a></li>
-                                            <li><a href="{{ $settings->facebook_link }}"><img src="img/icons/facebook.png" alt=""></a></li>
-                                        </ul>
-                                        <div class="red-line"></div>
-                                        <div class="text-red-line">
-                                            <p>{{ $settings->tel_num }}</p>
-                                            <p>{{ $settings->email }}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <script src="js/jquery-3.2.1.min.js"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-                <script src="js/owl.carousel.min.js"></script>
-                <script src="js/script.js"></script>
-            </footer>
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width">
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link rel="stylesheet" type="text/css" href="fonts/font.css">
+      <link rel="stylesheet" type="text/css" href="fonts/stylesheet.css">
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <title>RedRoom</title>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="block-header">
+          <div>
+            <img class="img-responsive" src="img/main_pic.png" alt="">
+          </div>
+          <div class="navbar">
+            <div class="nav-menu">
+              <div class="left-prev">
+                <img src="img/icons/left-prev.png" alt="" class="img-responsive">
+              </div>
+              <div class="menu_line">
+                <ul>
+                  <li><a href="#film">film</a></li>
+                  <li><a href="#film">video</a></li>
+                  <li><a href="#film">animation</a></li>
+                </ul>
+              </div>
+              <div class="right-prev">
+                <img src="img/icons/right-prev.png" alt="" class="img-responsive">
+              </div>
+            </div>
+            <div class="line-buttom">
+              <div class="left-buttom hovers btn-danger">
+                <a href="#latest">view projects</a>
+              </div>
+              <div class="right-buttom hovers btn-danger">
+                <a href="#form">hire us</a>
+              </div>
+            </div>
+          </div>
         </div>
-    </body>
+      </div>
+    </header>
+
+    @yield('main-content')
+
+    <footer>
+      <div class="footer-block">
+        <div class="container">
+          <div class="row no-gutters">
+            <div class="col-xs-12">
+              <div class="footer-text text-center">
+                <p>So What's Next?</p>
+                <h2>Let’s Work! <u>Just drop us a line.</u></h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="col-xs-12">
+            <div class="img-footer">
+              <div class="form gradient">
+                <form id="form">
+                  <p>Hello, my name is
+                    <input type="text" class="decorate" id="msgName" placeholder="first&last name">.</p>
+                  <p> I need a
+                    <input type="text" class="decorate" id="msgType" placeholder="films, video, animation,">.</p>
+                  <p>approximate duration around
+                    <input type="text" class="decorate" id="msgDuration" placeholder="xx:xx">.</p>
+                  <p>My budget is,
+                    <input type="text" class="decorate" id="msgBudget" placeholder="1000$">
+                  </p>
+                  <p>You can reach me on my phone at
+                    <input type="text" class="decorate" id="msgTelephone" placeholder="xxx-xxx-xxx">
+                  </p>
+                  <p>Or via email at
+                    <input type="text" class="decorate" id="msgEmail" placeholder="your@email.com">
+                  </p>
+                </form>
+                <div class="send-btn">
+                  <div class="send-img" onclick="sendEmail()">
+                    <img src="img/icons/air_send.png" alt="">
+                  </div>
+                </div>
+                <div class="line-icons-footer">
+                  <ul>
+                    <li><a href="{{ $settings->insta_link }}"><img src="img/icons/instagram.png" alt=""></a></li>
+                    <li><a href="{{ $settings->youtube_link }}"><img src="img/icons/youtube.png" alt=""></a></li>
+                    <li><a href="{{ $settings->facebook_link }}"><img src="img/icons/facebook.png" alt=""></a></li>
+                  </ul>
+                  <div class="red-line"></div>
+                  <div class="text-red-line">
+                    <p>{{ $settings->tel_num }}</p>
+                    <p>{{ $settings->email }}</p> 
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script src="js/jquery-3.2.1.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/script.js"></script>
+    </footer>
+  </body>
 </html>
